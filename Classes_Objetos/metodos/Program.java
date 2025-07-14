bf2489d91dbe6b29e7eae09c5222e64125fb1be9
Solange <solange_freitas01@hotmail.com>
2025-07-14 16:29:00 -0300

    Terceiro Commit

--- Classes_Objetos/metodos/Program.java
+++ Classes_Objetos/metodos/Program.java
@@ -1,24 +1,24 @@
 package Classes_Objetos.metodos;
 
-    public class Program {
-        public static void main(String[] args) {
-            PersonagemGame personagem1 = new PersonagemGame();
+public class Program {
+    public static void main(String[] args) {
+        PersonagemGame personagem1 = new PersonagemGame();
 
-            personagem1.setNome("Sonic");
-            personagem1.setSaudeAtual(10);
+        personagem1.setNome("Sonic");
+        personagem1.setSaudeAtual(10);
 
-            System.out.printf("%d\n", personagem1.getSaudeAtual());
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
 
-            personagem1.receberCura(91);
+        personagem1.receberCura(91);
 
-            System.out.printf("%d\n", personagem1.getSaudeAtual());
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
 
-            personagem1.tomarDano(25);
+        personagem1.tomarDano(25);
 
-            System.out.printf("%d\n", personagem1.getSaudeAtual());
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
 
-            personagem1.tomarDano(80);
+        personagem1.tomarDano(80);
 
-            System.out.printf("%d\n", personagem1.getSaudeAtual());
-        }
+        System.out.printf("%d\n", personagem1.getSaudeAtual());
     }
+}
