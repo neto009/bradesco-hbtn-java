e83e92e38d40cf480e68ea8dd4adf885be70bd34
Solange <solange_freitas01@hotmail.com>
2025-07-14 16:20:59 -0300

    Second Commit

---
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -0,0 +1,22 @@
+package Classes_Objetos.encapsulamento;
+
+public class PersonagemGame {
+    public int saudeAtual;
+    public String nome;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
