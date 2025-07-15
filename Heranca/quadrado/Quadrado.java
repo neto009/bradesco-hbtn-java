f8a1d95bb9fb178f9f375b863b43332b56016759
Solange <solange_freitas01@hotmail.com>
2025-07-15 08:52:44 -0300

    first commit

---
+++ Heranca/quadrado/Quadrado.java
@@ -0,0 +1,25 @@
+public class Quadrado extends  Retangulo{
+    private double lado;
+
+    public Quadrado(double lado) {
+        super();
+        this.lado = lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        if (lado <= 0) {
+            throw new IllegalArgumentException("Lado deve ser maior ou igual a 0");
+        }
+        this.lado = lado;
+        this.largura = lado;
+        this.altura = lado;
+    }
+
+    public String toString() {
+        return String.format("[Quadrado] %.2f", lado);
+    }
+}
