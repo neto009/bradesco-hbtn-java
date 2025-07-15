3403955f15e8400a7a5a5b1821867209953676f3
Solange <solange_freitas01@hotmail.com>
2025-07-15 14:36:37 -0300

    first commit

--- 0x03/livraria/Livro.java
+++ 0x03/livraria/Livro.java
@@ -24,21 +24,21 @@ public class Livro {
     }
 
     public void setTitulo(String titulo) throws LivroInvalidoException {
-        if (titulo == null || titulo.length() < 3) {
+        if (titulo == null || titulo.trim().length() < 3) {
             throw new LivroInvalidoException("Titulo de livro invalido");
         }
         this.titulo = titulo;
     }
 
     public void setAutor(String autor) throws AutorInvalidoException {
-        if (autor == null || autor.split(" ").length < 2) {
+        if (autor == null || autor.trim().split(" ").length < 2) {
             throw new AutorInvalidoException("Nome de autor invalido");
         }
         this.autor = autor;
     }
 
     public void setPreco(double preco) throws LivroInvalidoException {
-        if (preco <= 0) {
+        if (preco <= 0.0) {
             throw new LivroInvalidoException("Preco de livro invalido");
         }
         this.preco = preco;
