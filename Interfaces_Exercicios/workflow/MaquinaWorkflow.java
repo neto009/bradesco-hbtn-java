09220b0d3dd4e78c8671d70d14e72023197c4611
Solange <solange_freitas01@hotmail.com>
2025-07-15 11:27:47 -0300

    first commit

--- Interfaces_Exercicios/workflow/MaquinaWorkflow.java
+++ Interfaces_Exercicios/workflow/MaquinaWorkflow.java
@@ -1,3 +1,6 @@
+import atividades.Atividade;
+import java.util.List;
+
 public class MaquinaWorkflow {
     public void executar(Workflow workflow) {
         for (Atividade atividade : workflow.getAtividades()) {
