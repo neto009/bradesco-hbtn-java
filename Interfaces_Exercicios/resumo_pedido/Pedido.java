b32e4e0aebefd86486605ead887e2f31bce3a1c6
Solange <solange_freitas01@hotmail.com>
2025-07-15 11:49:23 -0300

    first commit

--- Interfaces_Exercicios/resumo_pedido/Pedido.java
+++ Interfaces_Exercicios/resumo_pedido/Pedido.java
@@ -6,6 +6,13 @@ public class Pedido {
     private double total;
     private Frete frete;
 
+    public Pedido(int codigo, double peso, double total, Frete frete) {
+        this.codigo = codigo;
+        this.peso = peso;
+        this.total = total;
+        this.frete = frete;
+    }
+
     public int getCodigo() {
         return codigo;
     }
