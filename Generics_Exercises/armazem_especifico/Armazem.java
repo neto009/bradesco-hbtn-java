5402fddf6b8e042e2586e91594fd58f2482c0dc2
Solange <solange_freitas01@hotmail.com>
2025-07-28 17:45:24 -0300

    Complexidade

--- Generics_Exercises/armazem_especifico/Armazem.java
+++ Generics_Exercises/armazem_especifico/Armazem.java
@@ -1,4 +1,4 @@
-public abstract class Armazem<T> {
+public abstract class Armazem<T> implements Armazenavel<T> {
     public abstract void adicionar(T item);
     public abstract T obter(String nome);
 }
