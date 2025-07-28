9f6e22afa24dc7fb47c2f6ca109943dcbf8cb1c3
Solange <solange_freitas01@hotmail.com>
2025-07-28 17:28:20 -0300

    Complexidade

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,13 @@
+package Generics.midias;
+
+public class Biblioteca<T extends Midia> {
+    private List<T> midias = new ArrayList<>();
+
+    public void adicionarMidia(T midia) {
+        midias.add(midia);
+    }
+
+    public List<T> obterListaMidias() {
+        return midias;
+    }
+}
