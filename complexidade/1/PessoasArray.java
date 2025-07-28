ec7371eda63fdaa5613db7503b00c7eb1717c3f9
Solange <solange_freitas01@hotmail.com>
2025-07-28 16:49:36 -0300

    Complexidade

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -19,13 +19,17 @@ public class PessoasArray {
 
     // implementar o método de buscaLinear
     public void buscaLinear(String nome){
+        boolean encontrado = false;
         System.out.println("Procurando pelo nome: \"" + nome + "\"");
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i] == nome) {
                 System.out.println("Nome pesquisado é " + nome + " que está na posição " + i);
+                encontrado = true;
             }
         }
-        throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        if (!encontrado){
+            throw new IllegalArgumentException("O nome " + nome + " não se encontra no array de nomes");
+        }
     }
 }
