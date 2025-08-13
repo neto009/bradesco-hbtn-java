aa9955e3af197732412730d796748ce1d7c8e17c
Solange <solange_freitas01@hotmail.com>
2025-08-12 22:46:51 -0300

    Lambdas

--- Streams_Collectors_Optional/maior_preco/Consulta.java
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -7,7 +7,14 @@ public class Consulta {
     public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
         return pedido.getProdutos()
             .stream()
-            .sorted(Comparator.comparing(Produto::getPreco).reversed())
+            .filter(produto -> produto.getCategoria() == CategoriaProduto.LIVRO)
             .collect(Collectors.toList());
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream()
+            .sorted(Comparator.comparing(Produto::getPreco).reversed())
+            .findFirst()
+            .orElse(null);
     }
 }
