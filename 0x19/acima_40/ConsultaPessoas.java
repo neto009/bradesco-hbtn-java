c2281a119c0eb542ed733f2acb73b8b4ff53b789
Solange <solange_freitas01@hotmail.com>
2025-08-14 15:14:13 -0300

    Collector

--- 0x19/acima_40/ConsultaPessoas.java
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -17,6 +17,6 @@ public class ConsultaPessoas {
     public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
         return pessoas.stream()
             .collect(Collectors.groupingBy(Pessoa::getCargo,
-            Collectors.mapping(Pessoa::getIdade, Collectors.toList())));
+                Collectors.filtering(pessoa -> pessoa.getIdade() > 40, Collectors.toList())));
     }
 }
